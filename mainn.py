a0d5e0ee7b75aae01aa6f8c75428d07a021082f3
ambroseotundo <ambroseotundo9@gmail.com>
2022-11-11 07:11:47 -0500

    feat:get audio, convert it to text, return the text in audio and create activation command

---
+++ mainn.py
@@ -0,0 +1,57 @@
+import speech_recognition as sr
+import gtts
+from playsound import playsound
+import os
+from datetime import datetime
+# from notion import NotionClient
+
+r = sr.Recognizer()
+
+
+ACTIVATION_COMMAND = "hey ambrose"
+
+def get_audio():
+    with sr.Microphone() as source:
+        print("Say something")
+        audio = r.listen(source)
+    return audio
+
+def audio_to_text(audio):
+    text = ""
+    try:
+        text = r.recognize_google(audio)
+    except sr.UnknownValueError:
+        print("Speech recognition could not understand audio")
+    except sr.RequestError:
+        print("could not request results from API")
+    return text
+
+def play_sound(text):
+    try:
+        tts = gtts.gTTS(text)
+        tempfile = "./temp.mp3"
+        tts.save(tempfile)
+        playsound(tempfile)
+        os.remove(tempfile)
+    except AssertionError:
+        print("could not play sound")
+
+
+
+if __name__ == "__main__":
+
+    while True:
+        a = get_audio()
+        command = audio_to_text(a)
+
+        if ACTIVATION_COMMAND in command.lower():
+            print("activate")
+            play_sound("What can I do for you at this time?")
+
+            note = get_audio()
+            note = audio_to_text(note)
+
+            if note:
+                play_sound(note)
+
+                
